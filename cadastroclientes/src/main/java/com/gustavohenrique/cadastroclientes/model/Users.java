006094becafe55e3819c3b28bf83de7cfc5cfab3
Gustavo <gustavosales0704@gmail.com>
2025-02-11 02:24:43 -0300

    :tada: Commit inicial

---
+++ cadastroclientes/src/main/java/com/gustavohenrique/cadastroclientes/model/Users.java
@@ -0,0 +1,56 @@
+package com.gustavohenrique.cadastroclientes.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuarios")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String usuario ;
+    private String email;
+    private String senha;
+
+    public Users(UUID id, String usuario, String email, String senha) {
+        this.id = id;
+        this.usuario = usuario;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
---
+++ cadastroclientes/src/test/java/com/gustavohenrique/cadastroclientes/CadastroclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavohenrique.cadastroclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
