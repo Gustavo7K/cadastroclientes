124cb898afcc4f3f094d47f9ad416fcfbfd7362d
Gustavo <gustavosales0704@gmail.com>
2025-02-15 03:41:46 -0300

    ✨ feat: Adicionando codigo no JWTService

--- cadastroclientes/src/main/java/com/gustavohenrique/cadastroclientes/infra/JwtService.java
+++ cadastroclientes/src/main/java/com/gustavohenrique/cadastroclientes/infra/JwtService.java
@@ -1,8 +1,15 @@
 package com.gustavohenrique.cadastroclientes.infra;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
@@ -11,8 +18,24 @@ public class JwtService {
     public JwtService(JwtEncoder encoder) {
         this.encoder = encoder;
     }
-
+    //montando o token modificado
     public String generateToken (Authentication authentication){
-        return null;  //"Return" temporario, pois tive que parar de programar nesse momento.
+        Instant now = Instant.now();
+        long expiry = 3600L; //tempo de expiração
+
+        String scopes = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" ")); //pegar os escopos e colocar no token
+
+        var claims = JwtClaimsSet.builder() //propriedades do token
+                .issuer("cadastroclientes")
+                .issuedAt(now) //quando foi emitido
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName()) //parei aqui
+                .claim("scope", scopes)
+                .build();
+
+        //retornar o token modificado
+        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue(); //pegar valor como string
     }
 }
