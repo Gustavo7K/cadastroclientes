476459a124b8338ad1acff94f24655d6e2f523ed
Gustavo <gustavosales0704@gmail.com>
2025-02-24 03:49:52 -0300

    :sparkles: feat: classe sql para adicionar o primeiro usuario ao banco de dados

---
+++ cadastroclientes/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+-- insert_data.sql
+INSERT INTO usuarios (id, usuario, email, senha) VALUES (uuid_generate_v4(), 'admin', 'admin@gmail.com' ,'$2a$10$GiseHkdvwOFr7A9KRWbeiOmg/PYPhWVjdm42puLfOzR/gIAQrsAGy');
